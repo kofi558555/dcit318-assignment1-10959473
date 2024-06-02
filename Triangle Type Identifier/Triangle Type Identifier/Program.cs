bcba861899583363ef640562ff9888ea81bd40c2
pkappiah014@st.ug.edu.gh <102973954+kofi558555@users.noreply.github.com>
2024-06-02 07:37:46 -0700

    Add files via upload
    3rd assignment

---
+++ Triangle Type Identifier/Triangle Type Identifier/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangle_Type_Identifier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the length of side A: ");
+            double sideA = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter the length of side B: ");
+            double sideB = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter the length of side C: ");
+            double sideC = Convert.ToDouble(Console.ReadLine());
+
+            string triangleType = GetTriangleType(sideA, sideB, sideC);
+            Console.WriteLine($"The triangle is {triangleType}.");
+        }
+
+        static string GetTriangleType(double a, double b, double c)
+        {
+            if (a == b && b == c)
+            {
+                return "Equilateral";
+            }
+            else if (a == b || b == c || a == c)
+            {
+                return "Isosceles";
+            }
+            else
+            {
+                return "Scalene";
+            }
+        }
+    }
+}
