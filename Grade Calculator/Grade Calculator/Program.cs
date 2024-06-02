53fbf0b0a5994e8149a17d7f1143d41ddef67f9a
pkappiah014@st.ug.edu.gh <102973954+kofi558555@users.noreply.github.com>
2024-06-02 07:36:14 -0700

    Add files via upload
    1st assignment

---
+++ Grade Calculator/Grade Calculator/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grade_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your numerical grade (0-100): ");
+            int grade = Convert.ToInt32(Console.ReadLine());
+
+            if (grade >= 90)
+            {
+                Console.WriteLine("Your Grade is: A");
+            }
+            else if (grade >= 80)
+            {
+                Console.WriteLine("Your Grade is: B");
+            }
+            else if (grade >= 70)
+            {
+                Console.WriteLine("Your Grade is: C");
+            }
+            else if (grade >= 60)
+            {
+                Console.WriteLine("Your letter grade is: D");
+            }
+            else
+            {
+                Console.WriteLine("Your Grade is: F");
+            }
+        }
+    }
+}
