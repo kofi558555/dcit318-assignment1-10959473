ee45382b27b7a45a68eac5b0b2336f7a1e844835
pkappiah014@st.ug.edu.gh <102973954+kofi558555@users.noreply.github.com>
2024-06-02 07:37:07 -0700

    Add files via upload
    2nd assignment

---
+++ Ticet Price Calculator/Ticet Price Calculator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ticet_Price_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int fullPrice = 10;
+            const int discountedPrice = 7;
+
+            Console.Write("Please enter your age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            int ticketPrice = (age >= 65 || age <= 12) ? discountedPrice : fullPrice;
+
+            Console.WriteLine($"The ticket price is: GHC{ticketPrice}");
+        }
+    }
+}
